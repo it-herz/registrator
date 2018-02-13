47056bc5b82d2b8f690c5fd7649b19c26ec3a5ed
user <user@consul1.hspu.local>
2018-02-13 16:20:49 +0300

    Reduced version (only with consul) with ipFromEnv support

--- modules.go
+++ modules.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	_ "github.com/gliderlabs/registrator/consul"
-	_ "github.com/gliderlabs/registrator/consulkv"
-	_ "github.com/gliderlabs/registrator/etcd"
-	_ "github.com/gliderlabs/registrator/skydns2"
-	_ "github.com/gliderlabs/registrator/zookeeper"
+	_ "github.com/it-herz/registrator/consul"
+	_ "github.com/it-herz/registrator/consulkv"
 )
