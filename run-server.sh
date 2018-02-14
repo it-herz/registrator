94f389f938db974a9c5f25b2c491514de77cf948
Dmitrii Zolotov <dzolotov@herzen.spb.ru>
2018-02-14 18:41:49 +0300

    Fix registrator entrypoint

--- run-server.sh
+++ run-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-consul agent enable-script-checks advertise=$(POD_IP) bind=0.0.0.0 bootstrap-expect=3 retry-join=consul-0.consul.$(NAMESPACE).svc.cluster.local retry-join=consul-1.consul.$(NAMESPACE).svc.cluster.local retry-join=consul-2.consul.$(NAMESPACE).svc.cluster.local client=0.0.0.0 config-file=/consul/config/server.json datacenter=dc data-dir=/consul/data domain=herzen encrypt=$(GOSSIP_ENCRYPTION_KEY) server ui disable-host-node-id &
-/bin/registrator
+consul agent -enable-script-checks -advertise=$(POD_IP) -bind=0.0.0.0 -bootstrap-expect=3 -retry-join=consul-0.consul.$(NAMESPACE).svc.cluster.local -retry-join=consul-1.consul.$(NAMESPACE).svc.cluster.local -retry-join=consul-2.consul.$(NAMESPACE).svc.cluster.local -client=0.0.0.0 -config-file=/consul/config/server.json -datacenter=dc -data-dir=/consul/data -domain=$(DOMAIN) -encrypt=$(GOSSIP_ENCRYPTION_KEY) -server -ui -disable-host-node-id &
+/bin/registrator -cleanup=true -resync=10 -ttl-refresh=60 -ttl=120 -internal -useIpFromEnv=POD_IP consul://localhost:8500
+
 
