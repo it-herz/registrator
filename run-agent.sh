e489b3b35b66e6d95e788ce741b14b89fe2dca54
Dmitrii Zolotov <dzolotov@herzen.spb.ru>
2018-03-20 23:26:52 +0300

    Fix run script (compat with helm stable/consul)

--- run-agent.sh
+++ run-agent.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/dumb-init /bin/sh
 set -e
-/usr/local/bin/docker-entrypoint.sh agent -enable-script-checks -advertise=$(POD_IP) -bind=0.0.0.0 -retry-join=consul-0.consul.$(NAMESPACE).svc.cluster.local -retry-join=consul-1.consul.$(NAMESPACE).svc.cluster.local -retry-join=consul-2.consul.$(NAMESPACE).svc.cluster.local -client=0.0.0.0 -config-file=/consul/config/server.json -datacenter=dc -data-dir=/consul/data -domain=$DOMAIN -encrypt=$(GOSSIP_ENCRYPTION_KEY) -ui -disable-host-node-id &
+/usr/local/bin/docker-entrypoint.sh agent -enable-script-checks -advertise=$(POD_IP) -bind=0.0.0.0 -retry-join=consul-consul-0.consul.$(NAMESPACE).svc.cluster.local -retry-join=consul-consul-1.consul.$(NAMESPACE).svc.cluster.local -retry-join=consul-consul-2.consul.$(NAMESPACE).svc.cluster.local -client=0.0.0.0 -datacenter=$DC -data-dir=/consul/data -domain=$DOMAIN -encrypt=$(GOSSIP_ENCRYPTION_KEY) &
 /bin/registrator -cleanup=true -resync=10 -ttl-refresh=60 -ttl=120 -internal -useIpFromEnv=POD_IP consul://localhost:8500
