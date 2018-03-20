8d286b2e414648232136c147760c95b99fe9d6b6
Dmitrii Zolotov <dzolotov@herzen.spb.ru>
2018-03-21 00:30:06 +0300

    Update run-agent.sh

--- run-agent.sh
+++ run-agent.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/dumb-init /bin/sh
 echo "Starting agent"
+mkdir -p /etc/consul
 echo "{\"encrypt\": \"$(echo -n $GOSSIP_ENCRYPTION_KEY | base64)\"}" > /etc/consul/encrypt.json
 GOSSIP_KEY="-config-file /etc/consul/encrypt.json"
 echo agent -enable-script-checks -advertise=$POD_IP -bind=0.0.0.0 -retry-join=consul-consul-0.consul.$NAMESPACE.svc.cluster.local -retry-join=consul-consul-1.consul.$NAMESPACE.svc.cluster.local -retry-join=consul-consul-2.consul.$NAMESPACE.svc.cluster.local -client=0.0.0.0 -datacenter=$DC -data-dir=/consul/data -domain=$DOMAIN $GOSSIP_KEY
