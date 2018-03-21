9343327aed17cf67e31a0e916bf087b086b4bd56
Dmitrii Zolotov <dzolotov@herzen.spb.ru>
2018-03-21 11:10:47 +0300

    Update run-agent.sh

--- run-agent.sh
+++ run-agent.sh
@@ -5,4 +5,12 @@ echo "{\"encrypt\": \"$(echo -n $GOSSIP_ENCRYPTION_KEY | base64)\"}" > /etc/cons
 GOSSIP_KEY="-config-file /etc/consul/encrypt.json"
 /usr/local/bin/docker-entrypoint.sh agent -enable-script-checks -advertise=$POD_IP -bind=0.0.0.0 -retry-join=consul-consul-0.consul-consul.$NAMESPACE.svc.cluster.local -retry-join=consul-consul-1.consul-consul.$NAMESPACE.svc.cluster.local -retry-join=consul-consul-2.consul-consul.$NAMESPACE.svc.cluster.local -client=0.0.0.0 -datacenter=$DC -data-dir=/consul/data -domain=$DOMAIN $GOSSIP_KEY &
 echo "Starting registrator"
-/bin/registrator -cleanup=true -resync=10 -ttl-refresh=60 -ttl=120 -internal -useIpFromEnv=POD_IP consul://localhost:8500
+declare -i c
+c=0
+while [ $c -lt 30 ]
+do
+  /bin/registrator -cleanup=true -resync=10 -ttl-refresh=60 -ttl=120 -internal -useIpFromEnv=POD_IP consul://localhost:8500
+  sleep 1
+  c+=1
+done
+echo "Error in initialization"
